8b8f39159d32434a809dac2b370f3ff041b94c0d
谢先金 <akc@ak.local>
2016-04-13 15:24:16 +0800

    clear the code

--- example/example.go
+++ example/example.go
@@ -4,8 +4,6 @@ import (
 	log "tes/cclog"
 	"time"
 	"fmt"
-	"os"
-	"path/filepath"
 )
 
 type kk struct  {
@@ -16,32 +14,31 @@ type kk struct  {
 func main()  {
 
 	k := kk{"shit", 12}
-	log.Trace("kksksksk","nothing",12,k)
+
+	fmt.Println("--------------before load config.json---------------",time.Now())
+	log.Trace("tes trace","nothing",12,k)
 	log.Info("tes info")
 	log.Debug("tes debug")
 	log.Warn("tes warn")
 	log.Error("tes error")
 	log.Critical("tes critical")
 
-	//time.Sleep(1 * time.Second)
-
-	fmt.Println("-----------------------------")
-	wd,e1 := filepath.Abs(filepath.Dir(os.Args[0]))
-	fmt.Println("当前路径:",wd,e1)
-
-	if err:=log.ConfigFromFile("/Users/akc/go/src/tes/cctes/log.json");err !=nil{
-		fmt.Println("加载配置失败:",err)
+	time.Sleep(1 * time.Second)
+	if err:=log.ConfigFromFile("config.json");err !=nil{
+		fmt.Println("fialed to load config.json:",err)
 	}
+	fmt.Println("--------------after load config.json----------------",time.Now())
 
 
-	log.Trace("kksksksk","nothing",12,k)
+	log.Trace("tes trace","nothing",12,k)
 	log.Info("tes-----------info")
 	log.Debug("tes-----------debug")
 	log.Warn("tes-----------warn")
 	log.Error("tes-----------error")
 	log.Critical("tes-----------critical")
 
-	fmt.Println("-----------------------------end")
+	time.Sleep(1 * time.Second)
+	fmt.Println("--------------tes formater--------------------------",time.Now())
 
 	log.Tracef("%v--%v--%v","Tracef",1, true)
 	log.Infof("%v--%v--%v","Infof",1, true)
@@ -50,5 +47,7 @@ func main()  {
 	log.Errorf("%v--%v--%v","Errorf",1, true)
 	log.Criticalf("%v--%v--%v","Criticalf",1, true)
 
+	// wait all log to be print
 	time.Sleep(1 * time.Second)
+
 }
